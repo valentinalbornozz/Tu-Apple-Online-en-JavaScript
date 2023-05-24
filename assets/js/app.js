52d129791c1cb5540a37cc4f3065ae6a58ef5f7f
Valentin Albornoz <valeg486@gmail.com>
2023-05-24 16:42:09 -0300

    update app.js
    actualizamos el menu de hamburguesa y x para que cuando el usuario apriete sobre un enlace del menu desplegable se guarde automaticamente y desplace a la misma vez

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,8 +60,8 @@ function smoothScroll(target) {
     document.body.scrollTop ||
     0;
   const distance = targetPosition - startPosition;
-  const duration = 500; // Duración del desplazamiento suave en milisegundos
-  const offset = 80; // Ajusta el valor para controlar la posición final
+  const duration = 500;
+  const offset = 80; 
 
   let startTime = null;
 
@@ -97,16 +97,36 @@ function smoothScroll(target) {
 
 /*Animación del menú hamburguesa*/
 document.querySelector(".bars__menu").addEventListener("click", () => {
-  var line1__bars = document.querySelector(".line1__bars-menu");
-  var line2__bars = document.querySelector(".line2__bars-menu");
-  var line3__bars = document.querySelector(".line3__bars-menu");
-  var container__menu = document.querySelector(".menu");
+  let line1__bars = document.querySelector(".line1__bars-menu");
+  let line2__bars = document.querySelector(".line2__bars-menu");
+  let line3__bars = document.querySelector(".line3__bars-menu");
+  let container__menu = document.querySelector(".menu");
 
   line1__bars.classList.toggle("activeline1__bars-menu");
   line2__bars.classList.toggle("activeline2__bars-menu");
   line3__bars.classList.toggle("activeline3__bars-menu");
 
   container__menu.classList.toggle("menu__active");
+});
+
+// Obtén todos los enlaces del menú
+let menuLinks = document.querySelectorAll(".menu a");
+
+// Agrega un evento de escucha para cada enlace
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    // Cierra el menú
+    let line1__bars = document.querySelector(".line1__bars-menu");
+    let line2__bars = document.querySelector(".line2__bars-menu");
+    let line3__bars = document.querySelector(".line3__bars-menu");
+    let container__menu = document.querySelector(".menu");
+
+    line1__bars.classList.remove("activeline1__bars-menu");
+    line2__bars.classList.remove("activeline2__bars-menu");
+    line3__bars.classList.remove("activeline3__bars-menu");
+
+    container__menu.classList.remove("menu__active");
+  });
 });
 
 /*Sección carrito y productos*/
